8186298e2638fbb26cdcf31a2b0995e2a25d3a2b
JurreBrandsen1709 <31656971+JurreBrandsen1709@users.noreply.github.com>
2017-10-31 16:02:09 +0100

    Created new code that can draw a grid and read the X and Y from a CSV file.
    The code will run from the main()
    Board(x,y,()) will draw the board
    set.gate(x,y) will init. the gates
    show_board() will init. the board.
    
    credit to:
    Westly White
    StackOverflow
    Jan 12 at 18:24

--- simpleGrid.py
+++ simpleGrid.py
@@ -1,8 +1,95 @@
-board = [ ['_'] *5 for i in range(5) ]
+import csv
 
+class Cell:
+    """
+    :param x: x-axis location
+    :param y: y-axis location
+    """
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.visual = "."
 
-for i, line in enumerate(board):
-	for char in line:
-		print char,
+    def __str__(self):
+        return self.visual
 
-	print
+class Board:
+
+    def __init__(self, x, y, show_labels=True):
+        """
+        :param x: How many columns the board uses
+        :param y: How many rows the board uses
+        :param show_labels: Display labels to the player
+        """
+
+        self.x = x
+        self.y = y
+        self.show_labels = show_labels
+        self.board = {}
+
+        self.generate_board()
+
+    def generate_board(self):
+        for y in range(0, self.y):
+
+            # Add the key X to the board dictionary
+            self.board[y] = []   
+
+            for x in range(0, self.x):
+                # Make a cell @ the current x, y and add it to the board
+                cell = Cell(x, y)
+                self.board[y].append(cell)
+
+    def show_board(self):
+
+        for key, cells in self.board.iteritems():
+
+            # Add the X Labels
+            if self.show_labels:
+                if key == 0:
+                    x_label = []
+                    for cell in self.board[key]:
+                        x_label.append(str(cell.x + 1))
+                    print " ".join(x_label)
+
+            row = []
+            for cell in cells:
+                row.append(str(cell))
+
+            # Add the Y labels
+            if self.show_labels:
+                row.append(str(cell.y + 1))
+
+            print " ".join(row)
+
+    def set_gate(self, x, y):
+        self.board[x][y].visual = "0"
+
+def main():
+	# Determine X and Y of the board
+	b = Board(10, 10, True)
+
+	# Open the CSV file 'gates.csv'
+	with open('gates.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+		    gate = row
+
+		    # retrieve X cord in csv-file
+		    gateX = ','.join(gate[1])
+
+		    # retrieve Y cord in csv-file
+		    gateY = ''.join(gate[2]).strip(")")
+		    
+		    # Turn the cord into intergers
+		    gateX = int(gateX)
+		    gateY = int(gateY)
+
+		    # Set a gate in the grid for every row in the csv-file
+		    b.set_gate(gateX, gateY)
+
+	# initialise board
+	b.show_board()
+
+if __name__ == '__main__':
+	main()
