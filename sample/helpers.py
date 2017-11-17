b574554355bb8ac2085ec747f77d1f7d61b96734
Lennart Klein <lennart.j.klein@gmail.com>
2017-11-17 15:43:09 +0100

    Placed comments to make clear how the algorithm works

--- sample/helpers.py
+++ sample/helpers.py
@@ -91,155 +91,55 @@ class Netlist:
 def calculatePath(board, aTuple, bTuple, label):
     '''
     Calculate route between two points
-    :param a: first point (tuple of coordinates)
-    :param b: second point (tuple of coordinates)
+    :param board: a Numpy array
+    :param aTuple: first point on the board (tuple of coordinates)
+    :param bTuple: second point on the board (tuple of coordinates)
+    :param label: the label this path gets on the board
     '''
 
-    
-    # # Pre-create coordinates
-    # a_left = list(aTuple)
-    # a_right = list(aTuple)
-    # a_up = list(aTuple)
-    # a_down = list(aTuple)
-    # counterLeft = 0
-    # counterRight = 0
-    # counterUp = 0
-    # counterDown = 0
+    print("Walking from gate: " + str(a) + " to " + str(b))
 
-
-    # Create comparable lists from tuples
+    # Create comparable lists from the point tuples
     a = list(aTuple)
     b = list(bTuple)
-    
-    print("Walking from gate: " + str(a) + " to " + str(b))
-    print("")
 
     # Initiate queue and constraints
     queue = [a]
     loops = 0
     found = False
 
+    # Algorithm
     while found == False:
 
+        # Track the progress
         print(str(queue))
-
         loops += 1
 
         # Pick first coordinate from the queue
         coord = queue.pop(0);
 
+        # Check if this coord is the destination
         if coord == b:
             found = True
-
         else:
-            # Create a list for the 4 adjacent cells of this coord   [2, 1, 0] [0, 1, 0] [1, 2, 0] [1, 0, 0] [1, 1, 1] [1, 1, -1]
+            # Create all the adjacent cells of this coord and add them to the queue
              for i, axes in enumerate(coord):
+
+                # Step forth on this axes
                 newCoordPlus = list(coord)
                 newCoordPlus[i] += 1
-                if not newCoordPlus in queue and newCoordPlus[0] > 0 and and newCoordPlus[1] > 0:
+                if not newCoordPlus in queue
+                   and newCoordPlus[0] > 0
+                   and newCoordPlus[1] > 0:
                     queue.append(newCoordPlus)
 
+                # Step back on this axes
                 newCoordMin = list(coord)
                 newCoordMin[i] -= 1
-                if not newCoordMin in queue and newCoordMinus[0] > 0 and and newCoordMinus[1] > 0:
+                if not newCoordMin in queue
+                   and newCoordMin[0] > 0
+                   and newCoordMin[1] > 0:
                     queue.append(newCoordMin)
 
-            #a_left[0] -= 1
-            #counterLeft += 1
-            #print("Step left: " + str(a_left))
-
-            # # If wall/gates/anything
-            # if board.board[a_left[2], a_left[1], a_left[0]] == 0:
-            #     print ("Free space found.")
-            #     print ("Appending a_left to queue")
-            #     # counterLeft += 1
-            #     # a_left.insert(3, counterLeft)
-            #     queue.append(a_left)
-            #     print ("counting step")
-            #     print ("queue is now:" + str(queue))
-            #     print("")
-
-            # else:
-            #     print("No free space here.")
-            #     print("")
-
-            #a_right[0] += 1
-            #counterRight += 1
-            #print("Step right: " + str(a_right))
-
-            # # If wall/gates/anything
-            # if board.board[a_right[2], a_right[1], a_right[0]] == 0:
-            #     print ("Free space found.")
-            #     print ("Appending a_right to queue")
-            #     # counterRight += 1
-            #     # a_right.insert(3, counterRight)
-            #     queue.append(a_right)
-            #     print ("counting step")
-            #     print ("queue is now:" + str(queue))
-            #     print("")
-
-            # else:
-            #     print("No free space here.")
-            #     print("")
-
-            # # # Y-axis
-            #a_up[1] -= 1
-            #counterUp -= 1
-            #print("Step up: " + str(a_up))
-
-            # # If wall/gates/anything
-            # if board.board[a_up[2], a_up[1], a_up[0]] == 0:
-            #     print ("Free space found.")
-            #     print ("Appending a_up to queue")
-            #     # counterUp += 1
-            #     # a_up.insert(3, counterUp)
-            #     queue.append(a_up)
-            #     print ("counting step")
-            #     print ("queue is now:" + str(queue))
-            #     print("")
-
-            # else:
-            #     print("No free space here.")
-            #     print("")
-
-            #a_down[1] += 1
-            #counterDown += 1
-            #print("Step down: " + str(a_down))
-
-            # # If wall/gates/anything
-            # if board.board[a_down[2], a_down[1], a_down[0]] == 0:
-            #     print ("Free space found.")
-            #     print ("Appending a_down to queue")
-            #     # counterDown += 1
-            #     # a_down.insert(3, counterDown)
-            #     queue.append(a_down)
-            #     print ("counting step")
-            #     print ("queue is now:" + str(queue))
-            #     print("")
-
-            # else:
-            #     print("No free space here.")
-            #     print("")
-
-            # # Z-axis
-
-
-            # if a_left == b_list:
-            #     print("")
-            #     print("Found the gate after " + str(counterLeft) + " steps")
-            #     break
-            # if a_right == b_list:
-            #     print("")
-            #     print("Found the gate after " + str(counterRight) + " steps")
-            #     break
-            # if a_up == b_list:
-            #     print("")
-            #     print("Found the gate after " + str(counterUp) + " steps")
-            #     break
-            # if a_down == b_list:
-            #     print("")
-            #     print("Found the gate after " + str(counterDown) + " steps")
-            #     break
-
-    print("GEVONUDDHHHHH")
+    print("Point B has been found!")
     print("Loops needed: " + str(loops))
