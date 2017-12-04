b209eeae55e554f3742ff8f1dfc802330705fd95
theetje <delange.thomas@gmail.com>
2017-12-04 13:19:30 +0100

    nieuwe classes en core
    Classes van netlist waar de log mee werkt en een variant van core.py waar die op is ingesteld

---
+++ sample/core_netlist_swap.py
@@ -0,0 +1,85 @@
+"""
+core.py
+Authors: - Jurre Brandsen
+         - Lennart Klein
+         - Thomas de Lange
+
+Pathfinder will find the most efficient path between two gates on a board.
+"""
+
+import settings
+
+import numpy as np
+import csv
+import classes
+from classes import Board
+from classes import Netlist
+from classes import Gate
+from classes import Netlist_log
+import colors as CLR
+
+def main():
+    '''
+    Initialise and draw a grid called Board
+    Read gate locations from gates file
+    '''
+
+    np.set_printoptions(threshold=np.nan)
+
+    # Make history log for used netlists
+    netlist_log = Netlist_log(1)
+    complete_list_found = False
+
+    counter = 0
+
+    while complete_list_found != True:
+
+        # Create a netlist and calculate path
+        netlist = Netlist(netlist_log.return_list())
+        # Initiate a board with a specified size
+        board = Board(settings.BOARD_WIDTH, settings.BOARD_HEIGHT, settings.BOARD_DEPTH)
+
+        # Read a CSV file for gate tuples
+        with open('data/gates'+ str(settings.FILE_GATES) + '.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            # print("Using gates file #" + str(settings.FILE_GATES))
+            # print("")
+
+            # Skip the header
+            next(reader, None)
+
+            for row in reader:
+                # Skip row if the data is commented
+                if row[0][:1] != '#':
+
+                    # Get the name of the gate
+                    gateLabel = int(row[0])
+
+                    # Fetch the coords X and Y
+                    gateX = int(row[1])
+                    gateY = int(row[2])
+                    gateZ = int(row[3])
+
+                    # Save gate object in gates list
+                    new_gate = Gate(netlist, gateLabel, gateX, gateY, gateZ)
+
+                    # Set a gate in the grid for every row in the file
+                    board.gatesObjects[gateZ, gateY, gateX] = new_gate
+                    board.gatesNumbers[gateZ, gateY, gateX] = gateLabel
+                    board.board[gateZ, gateY, gateX] = settings.SIGN_GATE
+
+        # Calculate the connections in this netlist
+        complete_list_found = netlist.execute_connections(board)
+
+        if complete_list_found != True:
+            new_netlist = netlist.switch_back_one(complete_list_found)
+            netlist_log.push_list([new_netlist])
+
+            if netlist_log.look_for_loop(netlist.list) == True:
+                new_netlist = netlist.switch_back_front(complete_list_found)
+                netlist_log.push_list([new_netlist])
+    # Plot the board
+    board.plot()
+
+if __name__ == '__main__':
+    main()
