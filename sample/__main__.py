fa9b8586e15951b67d054abd26eb86ed6dbfb347
Lennart Klein <lennart.j.klein@gmail.com>
2017-12-05 16:00:59 +0100

    Updated the __main__ with new code

--- sample/__main__.py
+++ sample/__main__.py
@@ -7,13 +7,13 @@ Authors: - Jurre Brandsen
 Pathfinder will find the most efficient path between two gates on a board.
 """
 
-
 import settings
 import colors as CLR
 
 from classes import Solution
 from classes import Gates
 from classes import Netlist
+
 # Print program settings
 print("")
 print("Using netlist: " + CLR.GREEN + str(settings.FILE_NETLIST) + CLR.DEFAULT)
@@ -23,14 +23,14 @@ print("")
 
 # Initiate a new netlist
 netlist = Netlist(settings.FILE_NETLIST)
-# netlist.sort_by_connection()
+netlist.sort_by_connection()
 
 # Initiate the gates
 gates = Gates(settings.FILE_GATES, settings.SIGN_GATE, netlist)
 
-#
+# 
 # COMMANDS TO OPTIMIZE NETLIST HERE
-#
+# 
 
 # Initiate a new solution
 solution = Solution()
@@ -39,3 +39,11 @@ solution.run(gates, netlist)
 # Plot solution information
 if settings.PLOT_SCORES:
     solution.plot_scores()
+
+# Plot solution information
+if settings.PLOT_RESULTS:
+    solution.plot_results()
+
+# Plot solution board
+if settings.PLOT_BEST:
+    solution.plot_best()
